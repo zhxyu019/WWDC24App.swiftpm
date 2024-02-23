2c5a619a3f1dda9ea9adda573c6db1afa77ad958
Ma Zhiyu <zhiyuma2006@gmail.com>
2024-02-23 23:53:36 +0800

    Added Changes to HomeView Layout

---
+++ CoreML/MLView.swift
@@ -0,0 +1,20 @@
+//
+//  MLView.swift
+//  WWDC24App
+//
+//  Created by Ma Zhiyu on 21/2/24.
+//
+
+import SwiftUI
+
+struct MLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MLView_Previews: PreviewProvider {
+    static var previews: some View {
+        MLView()
+    }
+}
