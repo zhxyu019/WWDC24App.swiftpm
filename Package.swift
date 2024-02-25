6ec5dc204d7c870a670f2bed4b68cd98ffcebc44
Ma Zhiyu <zhiyuma2006@gmail.com>
2024-02-26 00:15:24 +0800

    Added App Icon

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
             teamIdentifier: "FFH396A6Y4",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .location),
+            appIcon: .asset("AppIcon"),
             accentColor: .asset("AccentColor"),
             supportedDeviceFamilies: [
                 .pad,
