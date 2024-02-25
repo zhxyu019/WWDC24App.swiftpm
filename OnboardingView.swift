bb5b146432839ae258fa20ab74d5f4a0af0b9c85
Ma Zhiyu <zhiyuma2006@gmail.com>
2024-02-25 23:23:58 +0800

    Improved UI + Started on Report Feature

---
+++ OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  WWDC24App
+//
+//  Created by Ma Zhiyu on 25/2/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
